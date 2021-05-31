6ac18eabeb20490fc09c5747312fd1be3d4d4e1b
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2021-05-31 23:10:51 +0600

    #update

--- src/components/Videos/Video.jsx
+++ src/components/Videos/Video.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './_video.scss'
+
+import { AiFillEye } from 'react-icons/ai'
 
 const Video = () => {
     return (
-        <div>
-            video
-            
-        </div>
+        <div className='video' >
+         <div className='video__top'>
+             <img src="https://i.ytimg.com/vi/EKdAr3x7cBA/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBR4KKKlC2pCJPhyhJ2LdizsMmcRw" alt="" />
+          
+            <span className='video__top__duration'>5.04</span>
+         </div>
+         <div className='video__title'>Make an app in 5 minutes </div>
+         <div className='video__details'>
+            <span>
+               <AiFillEye /> 3m Views •{' '}
+            </span>{' '}
+            <span> 5 days ago </span>
+         </div>
+        
+            <div className='video__channel'>
+                <img src="https://yt3.ggpht.com/ytc/AAUvwnh8ycya-h_WkmP-BRoLIlY2oRF58swnKIcuH64O=s88-c-k-c0x00ffffff-no-rj" alt="" />
+              
+
+               <p>Rainbow</p>
+            </div>
+      
+      </div>
     )
 }
 
