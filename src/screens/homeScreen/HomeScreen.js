a3f5a18c9955e0df86a27b7dd891d03a6400a5dc
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2021-06-07 01:52:14 +0600

    #videos

--- src/screens/homeScreen/HomeScreen.js
+++ src/screens/homeScreen/HomeScreen.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import CategoryBar from "../../components/CatagoriesBar/CategoryBar";
 import GoToTop from "../../components/ScrollToTopButton";
 import Video from "../../components/Videos/Video";
+import { useDispatch } from 'react-redux'
+import { getPopularVideos } from "../../Redux/actions/videos.action";
 
 const HomeScreen = () => {
+
+   const dispatch = useDispatch()
+
+   useEffect(()=>{
+     dispatch(getPopularVideos())
+
+   },[dispatch])
   return (
     <Container>
       <CategoryBar />
